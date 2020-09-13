88f9fed5012ad387d59b4cc2bd244f8e738aad5f
Sunny <sv.sunnysharma@gmail.com>
2020-09-13 20:49:52 +0530

    Update auth.guard.ts

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate } from '@angular/router';
 
 import { AuthenticationService } from '../services/authentication.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService
     ) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const {token} = this.authenticationService.userValue;
+    canActivate() {
+        const {token} = this.authenticationService.userValue || {};
         if (token) {
-            return true;
+           return true;
         } else {
             this.router.navigate(['/login']);
             return false;
